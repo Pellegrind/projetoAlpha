7d0baf541a1ac37fa5e44a726410ff7a74f3d3ac
Pellegrind <rick_schil@hotmail.com>
2022-03-30 12:29:12 -0300

    player movement

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+    
+    private float movementX;
+    
+    private Rigidbody2D myBody;
+
+    private Animator anim;
+
+    private string Walk_Animation = "Walk";
+    
+    private SpriteRenderer sr;
+
+
+    private void Awake ()
+    {
+
+      myBody = GetComponent<Rigidbody2D>();
+      anim = GetComponent<Animator>();
+      sr = GetComponent<SpriteRenderer>();
+
+
+
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+        
+
+        
+    }
+}
